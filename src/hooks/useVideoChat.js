7b9fcd6bcc53f9642bb640cdd132ad3783f791c4
nguyenanh <vananh.nguyen1110@gmail.com>
2021-05-28 22:12:40 +0700

    change stun & turn sv

--- src/hooks/useVideoChat.js
+++ src/hooks/useVideoChat.js
@@ -83,10 +83,10 @@ const useVideoChat = (room, opts = { activeDevice: false }) => {
       config: {
         iceServers: [
           {
-            url: "stun:stun.l.google.com:19302",
+            urls: "stun:stun.l.google.com:19302",
           },
           {
-            url: "turn:13.59.253.25:3478",
+            urls: "turn:13.59.253.25:3478",
             credential: "key1",
             username: "username1",
           },
