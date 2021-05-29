2a14896e6ce7a16a692af9165b32495bd10a69e2
nguyenanh <vananh.nguyen1110@gmail.com>
2021-05-29 17:39:16 +0700

    change stun & turn sv

--- src/hooks/useVideoChat.js
+++ src/hooks/useVideoChat.js
@@ -87,8 +87,8 @@ const useVideoChat = (room, opts = { activeDevice: false }) => {
           },
           {
             urls: [
-              "turn:13.59.253.25:3478?transport=udp",
-              "turn:13.59.253.25:3478?transport=tcp",
+              "turn:172.31.2.63:3478?transport=udp",
+              "turn:172.31.2.63:3478?transport=tcp",
             ],
             credential: "rice241120",
             username: "anhnv",
