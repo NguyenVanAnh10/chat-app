afaa536a77e8c544138493494665abaedf0e91c5
nguyenanh <vananh.nguyen1110@gmail.com>
2021-06-12 13:14:33 +0700

    fix api url add friend

--- src/services/account.js
+++ src/services/account.js
@@ -3,4 +3,4 @@ import api from './api';
 export const getMe = () => api.GET('/me');
 export const login = params => api.POST('/login', params);
 export const logout = () => api.POST('/logout');
-export const addFriend = ({ userId, ...params }) => api.POST(`/${userId}/friends`, params);
+export const addFriend = ({ userId, ...params }) => api.POST(`/users/${userId}/friends`, params);
