d461916bd7798d16aa81e8d73c01fe9225e221cf
nguyenanh <vananh.nguyen1110@gmail.com>
2021-07-26 10:43:21 +0700

    api

--- src/model/index.js
+++ src/model/index.js
@@ -77,7 +77,8 @@ const models = {
       );
       return state;
     };
-    const composeEnhancers = (typeof window !== 'undefined'
+
+    const composeEnhancers = (process.env.NODE_ENV === 'development' && typeof window !== 'undefined'
         && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
       || compose;
 
